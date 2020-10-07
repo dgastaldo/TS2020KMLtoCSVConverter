1ec09601ebdb19eda2736c63b4743978036fe00b
Daniele Gastaldo <dinigasgas91@hotmail.it>
2020-10-07 19:43:20 +0200

    Aggiunto parsing di argomenti definiti da linea di comando.

--- TS2020KMLtoCSVConverter/Program.cs
+++ TS2020KMLtoCSVConverter/Program.cs
@@ -6,13 +6,107 @@ using System.Linq;
 
 namespace TS2020KMLtoCSVConverter
 {   
+    public static class Globalvariables
+    {
+        public static String Destinationpath = Path.Combine(Directory.GetCurrentDirectory(), "CSV");
+        public static String Csvfilenameroot = "";
+        public static String Logfilepath = Directory.GetCurrentDirectory();
+        public static String Logfilename = "log.txt";
+        public static String Originpath = Path.Combine(Directory.GetCurrentDirectory(), "KML");
+        public static String Originfilenamefilter = @"*.kml";
+        public static String Verbosity = "low";
+        public static String Logfile = Path.Combine(Globalvariables.Logfilepath, Globalvariables.Logfilename);
+
+        public static String[] GlobalVariablesValues()
+        {
+            String[] Values = { $"This log file is saved in the path {Globalvariables.Logfilepath} with the name {Globalvariables.Logfilename}.",
+                $"This log file verbosity is set to {Globalvariables.Verbosity}.",
+                $"Kml files origin path = {Globalvariables.Originpath}.",
+                $"Kml files name filter = {Globalvariables.Originfilenamefilter}.",
+                $"Csv files destination path = {Globalvariables.Destinationpath}.",
+                $"Csv files name root = {Globalvariables.Csvfilenameroot}."
+            };
+            return Values;
+        }
+
+        public static void RefreshDependents()
+        {
+            Globalvariables.Logfile = Path.Combine(Globalvariables.Logfilepath, Globalvariables.Logfilename);
+        }
+    }
     class Program
     {
-        static void Main(string[] args)
+        static void Main(String[] Args)
         {
-            //String OriginPath = @"C:\Users\afr\OneDrive\PROGETTO NODO DI TORINO\KML";
-            //String KmlFileName = "Località servite.kml";
-            //String Destinationpath = @"C:\Users\afr\OneDrive\PROGETTO NODO DI TORINO\CSV";
+            foreach (String Argument in Args)
+            {
+                String[] Splitargument = Argument.Split("=");
+                switch (Splitargument[0])
+                {
+                    case "-D":
+                    case "--pathforcsvfiles":
+                        Globalvariables.Destinationpath = Splitargument[1];
+                        break;
+                    case "-d":
+                    case "--csvfilenameroot":
+                        Globalvariables.Csvfilenameroot = Splitargument[1];
+                        break;
+                    case "-H":
+                    case "-h":
+                    case "-U":
+                    case "-u":
+                    case "--help":
+                    case "--usage":
+                        Program.Writetoconsole(Program.VersionString());
+                        Program.Writetoconsole(Program.UsageString());
+                        Environment.Exit(0);
+                        break;
+                    case "-L":
+                    case "--logfilepath":
+                        Globalvariables.Logfilepath = Splitargument[1];
+                        break;
+                    case "-l":
+                    case "--logfilename":
+                        Globalvariables.Logfilename = Splitargument[1];
+                        break;
+                    case "-O":
+                    case "--pathtokmlfiles":
+                        Globalvariables.Originpath = Splitargument[1];
+                        break;
+                    case "-o":
+                    case "--originfilenamefilter":
+                        Globalvariables.Originfilenamefilter = Splitargument[1];
+                        break;
+                    case "-V":
+                    case "--verbosity":
+                        Globalvariables.Verbosity = Splitargument[1];
+                        break;
+                    case "-v":
+                    case "--version":
+                        Program.Writetoconsole(Program.VersionString());
+                        Environment.Exit(0);
+                        break;
+                    default:
+                        Program.Writetoconsole(Program.VersionString());
+                        Program.Writetoconsole(Program.UsageString());
+                        Environment.Exit(0);
+                        break;
+                }
+            }
+            Globalvariables.RefreshDependents();
+            if (File.Exists(Globalvariables.Logfile))
+            {
+                File.Delete(Globalvariables.Logfile);
+            }
+            Program.Writetoconsole(Program.VersionString());
+            Program.Writetolog(Program.VersionString());
+            if (Globalvariables.Verbosity == "all" || Globalvariables.Verbosity == "debug")
+            {
+                Program.Writetoconsole(Program.UsageString());
+                Program.Writetolog(Program.UsageString());
+                Program.Writetoconsole(Globalvariables.GlobalVariablesValues());
+                Program.Writetolog(Globalvariables.GlobalVariablesValues());
+            }
             //var KmlFile = Path.Combine(OriginPath, KmlFileName);
             //Console.WriteLine($"Working on file {KmlFile}.");
             //XElement KmlContent = XElement.Load(KmlFile);
@@ -81,7 +175,50 @@ namespace TS2020KMLtoCSVConverter
             //        continue;
             //    }
             //}
-            Console.ReadKey();
+            //Console.ReadKey();
+        }
+
+        static String[] VersionString()
+        {
+            String[] Version =  { "Train Simulator 2020 KML to CSV Converter",
+                @"A C# Console Application by Daniele Gastaldo",
+                "for Train Simulator 2020 Downloadable Content (DLC) developers.",
+                "Version 1.0",
+                "For further informations, feature requests and bug reports",
+                "please contact the author at the e-mail adress",
+                @"daniele.gastaldo.1991@outlook.it"
+            };
+            return Version;
+        }
+        static String[] UsageString()
+        {
+            String[] Usage = { "Application usage help",
+                "An \"=\" symbol after the command means that an argument is required.",
+                "-D=, --pathforcsvfiles=: csv files output path;",
+                "-d=, --csvfilenameroot=: common csv filenames root;",
+                "-H, -h, -U, -u, --help, --usage: print this help;",
+                "-L=, --logfilepath=: log file path",
+                "-l=, --logfilename=: log file name. Must be completed with the .txt extension;",
+                "-O=, --pathtokmlfiles=: kml files input path;",
+                "-o=, --originfilenamefilter=: string used as a filter for the original kml files to be analyzed;",
+                "-V=, --verbosity=: log file verbosity. Must be one between \"low\" (default), \"debug\", \"benchmark\" or \"all\";",
+                "-v, --version: print version and author contact informations.",
+            };
+            return Usage;
+        }
+        static void Writetolog(String[] content)
+        {
+            if (File.Exists(Globalvariables.Logfile))
+                File.AppendAllLines(Globalvariables.Logfile, content);
+            else
+                File.WriteAllLines(Globalvariables.Logfile, content);
+        }
+        static void Writetoconsole(String[] content)
+        {
+            foreach (String line in content)
+            {
+                Console.WriteLine(line);
+            }
         }
     }
 }
